413f41cd92b8f796476cc4c839babd296ec144d8
martib9 <d.v.mokin@gmail.com>
2023-02-22 20:39:21 +0200

    meme

--- poem/script.js
+++ poem/script.js
@@ -1,70 +1,61 @@
-const inputWord = document.getElementById('input-word');
-const generateButton = document.getElementById('generate-button');
-const poemContainer = document.getElementById('poem');
+const form = document.getElementById('poem-form');
+const input = document.getElementById('poem-input');
 const errorMessage = document.getElementById('error-message');
+const poemContainer = document.getElementById('poem-container');
+const shareTwitter = document.getElementById('share-twitter');
+const shareTelegram = document.getElementById('share-telegram');
 
-generateButton.addEventListener('click', () => {
-  const word = inputWord.value.trim();
-  if (word.length === 0) {
-    errorMessage.innerText = 'Add word';
-    return;
-  }
-  if (word.length > 15) {
-    errorMessage.innerText = 'Word longer than 15 symbols';
-    return;
-  }
-  errorMessage.innerText = '';
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/api/poem');
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onload = () => {
-    if (xhr.status === 200) {
-      const poem = JSON.parse(xhr.responseText);
-      poemContainer.innerText = poem;
-    }
-  };
-  xhr.send(JSON.stringify({ word: word }));
-});
-
-// disable spaces
-inputWord.addEventListener('keydown', (event) => {
-  if (event.keyCode === 32) {
-    event.preventDefault();
-  }
-});
-
-// generate poem with ChatGPT
-async function generatePoem(word) {
-  const response = await fetch('/api/generate-poem', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ word: word }),
-  });
-  const data = await response.json();
-  return data.poem;
-}
-
-// handle form submission
-const form = document.querySelector('form');
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
-  const word = inputWord.value.trim();
-  if (word.length === 0) {
-    errorMessage.innerText = 'Add word';
+  errorMessage.textContent = '';
+
+  const word = input.value.trim();
+
+  if (word === '') {
+    errorMessage.textContent = 'Add word';
     return;
   }
+
+  if (word.includes(' ')) {
+    errorMessage.textContent = 'No spaces allowed';
+    return;
+  }
+
   if (word.length > 15) {
-    errorMessage.innerText = 'Word longer than 15 symbols';
+    errorMessage.textContent = 'Word longer than 15 symbols';
     return;
   }
-  errorMessage.innerText = '';
+
   try {
-    const poem = await generatePoem(word);
-    poemContainer.innerText = poem;
+    const response = await fetch('/api/generate-poem', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ word }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      poemContainer.textContent = data.poem;
+    } else {
+      errorMessage.textContent = data.error;
+    }
   } catch (error) {
     console.error(error);
-    errorMessage.innerText = 'Error generating poem';
+    errorMessage.textContent = 'Something went wrong';
   }
+});
+
+shareTwitter.addEventListener('click', () => {
+  const poem = poemContainer.textContent;
+  const url = `https://twitter.com/intent/tweet?text=${encodeURIComponent(poem)}`;
+  window.open(url, '_blank');
+});
+
+shareTelegram.addEventListener('click', () => {
+  const poem = poemContainer.textContent;
+  const url = `https://t.me/share/url?url=&text=${encodeURIComponent(poem)}`;
+  window.open(url, '_blank');
 });
