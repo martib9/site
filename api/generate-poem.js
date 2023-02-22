9065464b0973a2d0e9f463b8901931abc586c6e4
martib9 <d.v.mokin@gmail.com>
2023-02-22 21:09:19 +0200

    bebe

--- api/generate-poem.js
+++ api/generate-poem.js
@@ -1,43 +1,39 @@
-const { createCanvas } = require('canvas');
-const { GPT } = require('gpt-3');
 const config = require('../../config');
-
-const gpt = new GPT({
-  apiKey: config.gptApiKey,
-  engine: 'davinci',
-  prompt: config.gptPrompt,
-  temperature: 0.8,
-  maxTokens: 150,
-});
+const { createApi } = require('openai');
 
 module.exports = async (req, res) => {
-  const { word } = req.query;
+  const prompt = `${config.gptPrompt} ${req.query.word}`;
+  const temperature = 0.7;
+  const maxLength = 50;
+  const engine = 'davinci';
+  const completion = await createApi(config.gptApiKey).completions.create({
+    prompt,
+    temperature,
+    max_tokens: maxLength,
+    n: 1,
+    engine,
+  });
 
-  try {
-    const prompt = `Write a poem that rhymes with ${word}.`;
-    const result = await gpt.complete(prompt);
-    const poem = result.choices[0].text.trim();
-    const canvas = createCanvas(400, 300);
-    const ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = 'white';
-    ctx.font = '20px Arial';
-    const lines = poem.split('\n');
-    let y = 30;
-    for (const line of lines) {
-      ctx.fillText(line, 50, y);
-      y += 30;
-    }
-    const image = canvas.toDataURL('image/png');
-    res.json({
-      poem,
-      image,
-    });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({
-      error: 'An error occurred while generating the poem.',
-    });
+  console.log('API Key:', config.gptApiKey);
+  console.log('Prompt:', prompt);
+  console.log('Temperature:', temperature);
+  console.log('Max length:', maxLength);
+  console.log('Engine:', engine);
+  console.log('Completion:', completion);
+
+  if (completion.choices && completion.choices.length > 0) {
+    const text = completion.choices[0].text.trim();
+    const lines = text.split('\n').map((line) => line.trim());
+
+    const response = {
+      poem: lines.join('\n'),
+      image: `https://via.placeholder.com/300x150.png?text=${encodeURIComponent(
+        lines.join('\n')
+      )}`,
+    };
+
+    res.json(response);
+  } else {
+    res.status(400).json({ message: 'Could not generate poem.' });
   }
 };
