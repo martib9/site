2ce118d4e30f2440a7faa9baa0b3701ae4bf98c6
martib9 <d.v.mokin@gmail.com>
2023-02-22 20:21:40 +0200

    poen new

--- poem/script.js
+++ poem/script.js
@@ -32,3 +32,39 @@ inputWord.addEventListener('keydown', (event) => {
     event.preventDefault();
   }
 });
+
+// generate poem with ChatGPT
+async function generatePoem(word) {
+  const response = await fetch('/api/generate-poem', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ word: word }),
+  });
+  const data = await response.json();
+  return data.poem;
+}
+
+// handle form submission
+const form = document.querySelector('form');
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const word = inputWord.value.trim();
+  if (word.length === 0) {
+    errorMessage.innerText = 'Add word';
+    return;
+  }
+  if (word.length > 15) {
+    errorMessage.innerText = 'Word longer than 15 symbols';
+    return;
+  }
+  errorMessage.innerText = '';
+  try {
+    const poem = await generatePoem(word);
+    poemContainer.innerText = poem;
+  } catch (error) {
+    console.error(error);
+    errorMessage.innerText = 'Error generating poem';
+  }
+});
