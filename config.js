da0239e5ab650281b8e122599dd82d199a9ed6c6
martib9 <d.v.mokin@gmail.com>
2023-02-22 21:15:23 +0200

    meme

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-const config = {
-  OPENAI_API_KEY: "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"
-};
+module.exports = {
+    gptApiKey: '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',
+    gptPrompt: 'Write a poem that rhymes with',
+  };
+  
