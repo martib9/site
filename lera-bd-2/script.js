ecb2012953957cace8458f679fd42b224ee385ec
DM <martib24@yandex.ru>
2023-12-21 01:24:00 +0200

    sfad

---
+++ lera-bd-2/script.js
@@ -0,0 +1,4 @@
+// This script can be expanded as needed for further functionality
+document.querySelector('.scroll-button').addEventListener('click', function() {
+    // Scroll to the next screen logic here
+});
