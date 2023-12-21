7b8ceed2a631479fa996d9ba4df035392d99ca2e
DM <martib24@yandex.ru>
2023-12-21 15:59:48 +0200

    sticky

--- birth/script.js
+++ birth/script.js
@@ -26,3 +26,19 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var stickyButton = document.querySelector('.sticky-button');
+    var observer = new IntersectionObserver(function(entries) {
+        entries.forEach(function(entry) {
+            if (entry.isIntersecting) {
+                stickyButton.style.opacity = '1';
+            } else {
+                stickyButton.style.opacity = '0';
+            }
+        });
+    }, {threshold: 0.5});
+    
+    var targetScreen = document.getElementById('screen3');
+    observer.observe(targetScreen);
+});
