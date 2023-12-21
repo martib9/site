07234e1cf39ab510ce183cd9d3a59a18e3609581
DM <martib24@yandex.ru>
2023-12-21 11:45:39 +0200

    ff

--- birth/script.js
+++ birth/script.js
@@ -6,3 +6,13 @@ document.addEventListener('DOMContentLoaded', function() {
         document.querySelector('#screen2').scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const ctaButton = document.querySelector('.cta-button');
+
+    ctaButton.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent the default anchor behavior
+        const target = this.getAttribute('href');
+        document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
+    });
+});
