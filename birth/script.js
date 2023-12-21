399445434b759cc4823aa459de08b75b21e402d3
DM <martib24@yandex.ru>
2023-12-21 20:39:00 +0200

    asdf

--- birth/script.js
+++ birth/script.js
@@ -28,12 +28,20 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.sticky-button').forEach(button => {
-        button.addEventListener('click', function(e) {
-            e.preventDefault();
-            const target = this.getAttribute('href');
-            document.querySelector(target).scrollIntoView({ behavior: 'smooth' });
-        });
+    const stickyButton = document.querySelector('.sticky-button');
+    const screen3 = document.querySelector('#screen3');
+    
+    window.addEventListener('scroll', function() {
+        const screen3Top = screen3.offsetTop;
+        const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (scrollPosition >= screen3Top) {
+            stickyButton.style.display = 'block'; // Show the button
+            stickyButton.style.opacity = 1; // Fade in
+        } else {
+            stickyButton.style.opacity = 0; // Fade out
+            setTimeout(() => { stickyButton.style.display = 'none'; }, 500); // Hide after fade out
+        }
     });
 });
 
