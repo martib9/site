9b00ff78dc947db6921c165afaad32ca77097230
DM <martib24@yandex.ru>
2025-05-18 17:22:27 +0300

    fix?

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,3 @@
-import '../styles/globals.css';
-import Head from 'next/head';
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
-        <title>LxD Budget</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
-
 // File: pages/index.js
 import { useState, useEffect } from 'react';
 import InputNumeric from '../components/InputNumeric';
