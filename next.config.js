3d2ddecfa0de12136f5229115e58791c304d2be3
DM <martib24@yandex.ru>
2025-05-18 15:38:28 +0300

    Integrate budget PWA scaffold at root; remove old index.html

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/budget',
-  assetPrefix: '/budget/',
+  reactStrictMode: true
 };
-
 module.exports = nextConfig;
