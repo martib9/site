143d3705311187d7fa1e3187c9e1aab4e4b6e144
DM <martib24@yandex.ru>
2025-08-05 11:26:27 +0300

    feat: add we page

---
+++ pages/we.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function We() {
+  return (
+    <>
+      <Head>
+        <title>We</title>
+      </Head>
+      <div className="p-8">
+        <h1 className="text-2xl font-semibold mb-4">We</h1>
+        <p>Welcome to the we page.</p>
+      </div>
+    </>
+  );
+}
